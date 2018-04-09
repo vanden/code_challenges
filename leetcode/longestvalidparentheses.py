e0c7b4d66ad6df27510a5d02e73218317dfce71b
Brian van den Broek <vanden@gmail.com>
2018-04-09 15:16:33 -0700

    Add still too slow leetcode solution

--- leetcode/longestvalidparentheses.py
+++ leetcode/longestvalidparentheses.py
@@ -2,40 +2,30 @@
 
 #https://leetcode.com/problems/longest-valid-parentheses/description/
 
-# AGAIN, Still too slow. Still passing 217/229 cases.
+# *AGAIN*, Still too slow. Still passing 227/229 cases.
 class Solution:
     def longestValidParentheses(self, string):
         """
         :type s: str
         :rtype: int
         """
-        # Restrict to substrings of even length as only they can be valid.
-        startLength = int(len(string)/2) * 2
-
-        # Length in descending order as once we find a valid string of a
-        # length considered in that order, we know that we have found the
-        # longest such string.
-        for length in range(startLength, 0, -2):
-
-            for start in range(0,len(string)-length+1):
-
-                substring = string[start:start+length]
-                parenCount = 0
-                
-                for c in substring:
-                    if c == '(':
-                        parenCount += 1
-                    elif c == ')':
-                        parenCount -= 1
-
-                    if parenCount < 0:
-                        break
+        maxLen = 0
+        for start in range(len(string)):
+            index = start
+            parenCount = 0
+            while index < len(string):
+                if string[index] == '(':
+                    parenCount += 1
                 else:
-                    if parenCount == 0:
-                        return len(substring)
-        return 0
+                    parenCount -= 1
+                if parenCount < 0:
+                    break
+                if parenCount == 0:
+                    maxLen = max(maxLen, index - start + 1)
+                index += 1
+        return maxLen
 
-# s = Solution()
-# t = ")(((((()())()()))()(()))("
+#s = Solution()
+#t = ")(((((()())()()))()(()))("
 # #t = '(())))'
-# print(s.longestValidParentheses(t))
+#print(s.longestValidParentheses(t))
