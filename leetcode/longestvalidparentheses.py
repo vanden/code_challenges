2d997377daa56c1b6dd56464802a87f241bece03
Brian van den Broek <vanden@gmail.com>
2018-04-08 18:33:46 -0700

    Add a still too slow leetcode solution

--- leetcode/longestvalidparentheses.py
+++ leetcode/longestvalidparentheses.py
@@ -2,33 +2,40 @@
 
 #https://leetcode.com/problems/longest-valid-parentheses/description/
 
-# Still too slow. Now, pass 217/229 cases.
+# AGAIN, Still too slow. Still passing 217/229 cases.
 class Solution:
-    def longestValidParentheses(self, s):
+    def longestValidParentheses(self, string):
         """
         :type s: str
         :rtype: int
         """
         # Restrict to substrings of even length as only they can be valid.
-        startLength = int(len(s)/2) * 2
+        startLength = int(len(string)/2) * 2
 
+        # Length in descending order as once we find a valid string of a
+        # length considered in that order, we know that we have found the
+        # longest such string.
         for length in range(startLength, 0, -2):
 
-            for start in range(0,len(s)-length+1):
-                substring = s[start:start+length]
-                if self.isBalanced(substring):
-                    return len(substring)
+            for start in range(0,len(string)-length+1):
+
+                substring = string[start:start+length]
+                parenCount = 0
+                
+                for c in substring:
+                    if c == '(':
+                        parenCount += 1
+                    elif c == ')':
+                        parenCount -= 1
+
+                    if parenCount < 0:
+                        break
+                else:
+                    if parenCount == 0:
+                        return len(substring)
         return 0
 
-    def isBalanced(self, s):
-        stack = []
-        for c in s:
-            if c == '(':
-                stack.append(c)
-            elif c == ')':
-                if not stack:
-                    return False
-                last = stack.pop()
-                if last != '(':
-                    return False
-        return len(stack) == 0
+# s = Solution()
+# t = ")(((((()())()()))()(()))("
+# #t = '(())))'
+# print(s.longestValidParentheses(t))
