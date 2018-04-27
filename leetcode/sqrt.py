3ba20353c9d6f2817d2d5708292568384b1d563e
Brian van den Broek <vanden@gmail.com>
2018-04-26 18:24:20 -0700

    Add slow leetcode solution

---
+++ leetcode/sqrt.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/sqrtx/description/
+
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(x**0.5)
+        
