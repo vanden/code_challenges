4bf37f64f4f672b2758186b1884c61e5a9f41966
Brian van den Broek <vanden@gmail.com>
2018-05-03 22:49:05 -0700

    Add much faster leetcode solution

---
+++ leetcode/judge_route_circle.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/judge-route-circle/description/
+
+class Solution:
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        return (moves.count('U') == moves.count('D') and
+                moves.count('L') == moves.count('R'))
