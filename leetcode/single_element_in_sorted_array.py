baa34d1ae557f8d76acfc2e2ba29ac193c941067
Brian van den Broek <vanden@gmail.com>
2018-05-07 16:46:36 -0700

    Add leetcode solution respecting space constraints (slower)

--- leetcode/single_element_in_sorted_array.py
+++ leetcode/single_element_in_sorted_array.py
@@ -18,21 +18,48 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        print(nums)
-        if len(nums) == 1:
-            return nums[0]
-        mid = len(nums)//2
-        if mid % 2 == 0:
-            mid += 1
-        print(mid, nums[mid-1:mid+2])
+        start = 0
+        end = len(nums) - 1
+        c = 0
+        while start != end:
+            mid = (start + end) // 2
+            if mid % 2:
+                if nums[mid] == nums[mid+1]:
+                    # Then, unpaired is left
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if nums[mid] == nums[mid+1]:
+                    # Unpaired right
+                    start = mid
+                else:
+                    end = mid
+        return nums[start]
 
-        if nums[mid] == nums[mid-1]:
-            return self.singleNonDuplicate(nums[mid+1:])
-        else:
-            return self.singleNonDuplicate(nums[:mid])
 
+s = Solution()
+print()
+d = [1, 2, 2, 3, 3,]
+print(s.singleNonDuplicate(d))
 
-# s = Solution()
+d = [1, 1, 2, 3, 3,]
+print(s.singleNonDuplicate(d))
 
-# d = [1, 1, 2, 3, 3,]
-# s.singleNonDuplicate(d)
+d = [1, 1, 2, 2, 3,]
+print(s.singleNonDuplicate(d))
+
+print()
+
+d = [1, 2, 2, 3, 3, 4, 4]
+print(s.singleNonDuplicate(d))
+
+d = [1, 1, 2, 3, 3, 4, 4]
+print(s.singleNonDuplicate(d))
+
+d = [1, 1, 2, 2, 3, 4, 4]
+print(s.singleNonDuplicate(d))
+
+d = [1, 1, 2, 2, 3, 3, 4]
+print(s.singleNonDuplicate(d))
+
