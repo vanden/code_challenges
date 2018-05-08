5d7f98eae0f3b685c97f18c418442750ef9d3281
Brian van den Broek <vanden@gmail.com>
2018-05-08 13:50:51 -0700

    Add slow leetcode solution

---
+++ leetcode/width_of_bintree.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/maximum-width-of-binary-tree/description/
+
+class Solution:
+    def widthOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        maxWidth = -1
+        toExplore = [root]
+
+        while any(toExplore):
+            current = toExplore
+            toExplore = []
+            levelVals = []
+            for node in current:
+                if node:
+                    levelVals.append(node.val)
+                    toExplore.extend([n for n in [node.left, node.right]])
+                else:
+                    if levelVals:
+                        # Don't put Nones in front as they aren't spacers
+                        levelVals.append(None)
+                    if toExplore:
+                        # Similar reason; cuts down search space.
+                        toExplore.extend([None, None])
+            while levelVals and levelVals[-1] is None:
+                # Remove non-sapcers
+                levelVals.pop()
+            while toExplore and toExplore[-1] is None:
+                # Cut search space
+                toExplore.pop()
+            maxWidth = max(maxWidth, len(levelVals))
+        return maxWidth
