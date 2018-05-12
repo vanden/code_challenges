5d5876744aadfafcde815e0420653314fc8733b0
Brian van den Broek <vanden@gmail.com>
2018-05-11 18:36:47 -0700

    Add upsetting leetcode solution

--- leetcode/bulb_switcher.py
+++ leetcode/bulb_switcher.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/bulb-switcher/description/
 
-from collections import defaultdict
+import math
 
 class Solution(object):
     def bulbSwitch(self, n):
@@ -8,19 +8,4 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # Not sure this is an improvement, but still times out on same case.
-        if n == 0:
-            return 0
-
-        toggle_counts = defaultdict(int)
-
-        for i in range(1, n+1):
-            for bulbIdx in range(n):
-                if not (bulbIdx + 1) % i:
-                    toggle_counts[bulbIdx] += 1
-
-        return len([c for c in toggle_counts.values() if c%2])
-
-
-s = Solution()
-s.bulbSwitch(1)
+        return int(math.sqrt(n))
