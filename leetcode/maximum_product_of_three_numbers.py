8ddc93f9ecf2cfaf41f41e25beed0e9c565b8ba6
Brian van den Broek <vanden@gmail.com>
2018-05-12 14:41:20 -0700

    Add faster leetcode solution

--- leetcode/maximum_product_of_three_numbers.py
+++ leetcode/maximum_product_of_three_numbers.py
@@ -1,16 +1,40 @@
 # https://leetcode.com/problems/maximum-product-of-three-numbers/description/
 
+# Uglier than the sorting version. But, somewhat simpler and O(n) and O(1)
+# rather than O(nlogn) and O(n). (I think the sorting version is O(n) because
+# while the input nums got replaced by the sorted version, the caller would
+# still retain a reference, so there was a new O(n) memory allocation.
+
+
 class Solution:
     def maximumProduct(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        candidates = [nums.pop()]
-        if nums[0]*nums[1] > nums[-1] * nums[-2]:
-            candidates.extend(nums[:2])
-        else:
-            candidates.extend(nums[-2:])
+        firsts = sorted(nums[0:3])
+        max1, max2, max3 = firsts[::-1]
+        min1, min2 = firsts[0:2]
 
-        return candidates[0] * candidates[1] * candidates[2]
+        for idx, num in enumerate(nums):
+            if idx < 3:
+                continue
+            if num < min2 and num >= min1:
+                min2 = num
+            elif num < min1:
+                min2 = min1
+                min1 = num
+            if num > max3 and num <= max2:
+                max3 = num
+            elif num > max2 and num <= max1:
+                max3 = max2
+                max2 = num
+            elif num > max1:
+                max3 = max2
+                max2 = max1
+                max1 = num
+        return max(max1 * min1 * min2, max1 * max2 * max3)
+
+# d = [1,2,3,2]
+# s = Solution()
+# print(s.maximumProduct(d))
