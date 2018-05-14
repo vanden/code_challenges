e76c669cc88468787e0610dd8a8220924ab18b41
Brian van den Broek <vanden@gmail.com>
2018-05-14 14:41:39 -0700

    Add perhaps faster leetcode, still O(n) memory

--- leetcode/single_number.py
+++ leetcode/single_number.py
@@ -13,6 +13,20 @@ class Solution:
         for num in nums:
             seen[num] += 1
 
-        for key in seen:
-            if seen[key] == 1:
-                return key
+        # for key in seen:
+        #     if seen[key] == 1:
+        #         return key
+
+        for k, v in seen.items():
+            if v == 1:
+                return k
+
+        # The problem spec doesn't give the needed guarantees for this to
+        # work, but let's see. And, indeed, there are test cases for which it
+        # fails.
+        #
+        # for num in nums:
+        #     nums[abs(num)-1] *= -1
+        # for num in nums:
+        #     if nums[abs(num)-1] < 0:
+        #         return abs(num)
