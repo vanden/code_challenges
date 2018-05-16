4b7fae00c94b36d33e3da2973ad8eb7d0a337e8d
Brian van den Broek <vanden@gmail.com>
2018-05-15 23:38:40 -0700

    Add leetcode solution

---
+++ leetcode/longest_uncommon_subsequenceI.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/longest-uncommon-subsequence-i/description/
+
+# Stupid problem
+
+class Solution:
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        if a == b:
+            return -1
+        return max(len(a), len(b))
