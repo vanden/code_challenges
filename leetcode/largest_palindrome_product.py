d6f18f35817ca625c04a6f38cbddf870867fad2f
Brian van den Broek <vanden@gmail.com>
2019-02-09 02:24:51 -0800

    Add old WIP leetcode

--- leetcode/largest_palindrome_product.py
+++ leetcode/largest_palindrome_product.py
@@ -8,26 +8,20 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        upperBound = 10**n-1
-        lowerBound = 10**n
+        upperBound = (10**n)-1
+        lowerBound = 10**(n-1)
+        for candidate in range(upperBound**2, (lowerBound-1)**2, -1):
+            if not isPalindrome(candidate):
+                continue
 
-        # Naive thing without any effort at analysis of what a factor of a
-        # palindrome must be like.
-        #
-        # Oh, but wait. This is dumb. Surely is is
-        # better to start with the palidromic numbers and find one that
-        # factors as desired. Still, let's run. I predict a timeout.
-
-        # Indeed, for n = 7 run as a single test, it times out.
-        largest = float('-inf')
-        for x in range(upperBound, lowerBound-1, -1):
-            for y in range(x, lowerBound-1, -1):
-                if x*y <= largest:
-                    break
-                if self.isPalindrome(x * y):
-                    largest = x * y
-                    break
-        return largest % 1337
+            for i in range(upperBound, lowerBound-1, -1):
+                if candidate % i == 0:
+                    if lowerBound < (candidate // i) < upperBound:
+                        return candidate % 1337
 
     def isPalindrome(self, n):
         return n == int(''.join(reversed(str(n))))
+
+
+s = Solution()
+s.largestPalindrome(7)
