29f59d37c0576c015167821f191b5dba50c065a5
Brian van den Broek <vanden@gmail.com>
2019-02-09 17:35:19 -0800

    Add passing, but slow leetcode solution

---
+++ leetcode/partion_lables.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/partition-labels/description/
+
+# A string S of lowercase letters is given. We want to partition this string
+# into as many parts as possible so that each letter appears in at most one
+# part, and return a list of integers representing the size of these parts.
+
+
+# First version:
+
+# Runtime: 408 ms, faster than 1.97% of Python3 online submissions for
+# Partition Labels.
+
+# Memory Usage: 6.4 MB, less than 80.75% of Python3 online submissions for
+# Partition Labels.
+
+# So, slow, but relatively little memory used. Obviously, trade space for time
+# and memoize rindex results
+
+class Solution:
+
+    def partitionLabels(self, string):
+        chunkLengths = []
+        i = 0
+        lastIndex = -1
+        while i < len(string):
+            endIdx = string.rindex(string[i])
+            j = max(string.rindex(c) for c in string[lastIndex+1:endIdx+1])
+            if i == j:
+                chunkLengths.append(i-lastIndex)
+                lastIndex = i
+                i += 1
+            else:
+                i += 1
+        return chunkLengths
+
+
+
+s = Solution()
+s.partitionLabels("ababcbacadefegdehijhklij")
+s.partitionLabels("avbabcbacadedehijhklvij")
+s.partitionLabels("avbabvij")
