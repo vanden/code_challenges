c90d36333c60383028c794d10a9f6a39bfed86d4
Brian van den Broek <vanden@gmail.com>
2019-02-09 16:01:20 -0800

    Add incorrect hackerrank solution

--- hacker_rank/fraudulent_activity_notifications.py
+++ hacker_rank/fraudulent_activity_notifications.py
@@ -2,23 +2,46 @@
 
 from collections import deque
 
+class Median():
+    def __init__(self, seed):
+        self.size = len(seed)
+        self.deque = deque(seed, len(seed))
+        self.counts = [0] * 201
+        for val in seed:
+            self.counts[val] += 1
+
+    def push(self, val):
+        self.counts[self.deque[0]] -= 1
+        self.deque.append(val)
+        self.counts[val] += 1
+
+    def median(self):
+        midpoint = int(self.size)/2
+        totalCountSeen = 0
+        if self.size % 2:
+            for i, count in enumerate(self.counts):
+                totalCountSeen += count
+                if totalCountSeen >= midpoint:
+                    return i
+        else:
+            low = None
+            for i, count in enumerate(self.counts):
+                totalCountSeen += count
+                if low is None and (totalCountSeen >= midpoint):
+                    low = i
+                elif totalCountSeen >= (midpoint + 1):
+                    return (low + i)/2
+
 def activityNotifications(expenditure, d):
-    window = deque(expenditure[:d], d)
+    window = Median(expenditure[:d])
     notifications = 0
+
     for day in expenditure[d:]:
-        median = getMedian(window)
-        if day >= 2 * median:
+        median = window.median()
+        if day >= (2 * median):
             notifications += 1
-        window.append(day)
+        window.push(day)
     return notifications
-
-def getMedian(window):
-    spends = sorted(window)
-    length = len(spends)
-    if length%2:
-        return spends[int(length/2)]
-    return (spends[int(length/2)] + spends[int(length/2)-1])/2
-
 
 case0 = ([2, 3, 4, 2, 3, 6, 8, 4, 5], 5, 2)
 case1 = ([1, 2, 3, 4, 4], 4, 0)
@@ -30,5 +53,9 @@ def test(case):
 
 
 if __name__ == "__main__":
-    for case in [case0, case1, case2]:
+    for case in [
+            case0,
+            case1,
+            case2
+    ]:
         test(case)
