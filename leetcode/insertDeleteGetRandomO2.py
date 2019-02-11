e42fbfa354917c245aee6c1b92e93e8beda9cd98
Brian van den Broek <vanden@gmail.com>
2019-02-11 09:22:27 -0800

    Add unsuccessful memory optimization attempt

--- leetcode/insertDeleteGetRandomO2.py
+++ leetcode/insertDeleteGetRandomO2.py
@@ -10,7 +10,7 @@
 import random
 from collections import defaultdict
 
-class RandomizedCollection:
+class RandomizedCollection1:
 
     def __init__(self):
         """
@@ -47,6 +47,76 @@ class RandomizedCollection:
         self.data[valPosition] = None
 
         return True
+
+    def getRandom(self) -> 'int':
+        """
+        Get a random element from the collection.
+        """
+        choice = None
+        while choice is None:
+            choice = random.choice(self.data)
+        return choice
+
+# Attempted memory optimization did not help:
+
+# Runtime: 92 ms, faster than 95.42% of Python3 online submissions for Insert
+# Delete GetRandom O(1) - Duplicates allowed.
+
+# Memory Usage: 12.2 MB, less than 79.25% of Python3 online submissions for
+# Insert Delete GetRandom O(1) - Duplicates allowed
+
+
+class RandomizedCollection:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data = []
+        self.positions = defaultdict(list)
+        self.counts = defaultdict(int)
+
+        # Attempting a memory optimization. This should help if inserts are
+        # somewhat more frequent than removes. But, in a real world
+        # application, it would need instrumenting to see if it is better.
+        self.freeSlots = []
+
+    def __repr__(self):
+        return "RandomizedCollection(data: %s, positions: %s)" %(self.data, self.positions)
+
+
+    def insert(self, val: 'int') -> 'bool':
+        """
+        Inserts a value to the collection. Returns true if the collection did
+        not already contain the specified element.
+        """
+
+        novel = not (val in self.positions and self.positions[val])
+
+        if self.freeSlots:
+            slot = self.freeSlots.pop()
+            self.data[slot] = val
+            self.positions[val].append(slot)
+        else:
+            self.data.append(val)
+            self.positions[val].append(len(self.data) - 1)
+
+        return novel
+
+
+    def remove(self, val: 'int') -> 'bool':
+        """
+        Removes a value from the collection. Returns true if the collection
+        contained the specified element.
+        """
+        if not (val in self.positions and self.positions[val]):
+            return False
+
+        valPosition = self.positions[val].pop()
+        self.data[valPosition] = None
+        self.freeSlots.append(valPosition)
+        return True
+
 
     def getRandom(self) -> 'int':
         """
