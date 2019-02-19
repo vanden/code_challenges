97821d6beb89391320affc1499d752a796b1927a
Brian van den Broek <vanden@gmail.com>
2019-02-18 21:58:03 -0800

    Improve naming and whitespace

--- leetcode/min_cost_climbing_stairs.py
+++ leetcode/min_cost_climbing_stairs.py
@@ -8,13 +8,16 @@
 
 class Solution:
     def minCostClimbingStairs(self, cost: 'List[int]') -> 'int':
+
         if len(cost) < 3:
             return min(cost)
-        stairCosts = [0,0]
+        
+        totalCosts = [0,0]
+        
         for i in range(2, len(cost)):
-            stairCosts.append(min(
-                (stairCosts[i-1] + cost[i-1]),
-                (stairCosts[i-2] + cost[i-2])))
+            totalCosts.append(min(
+                (totalCosts[i-1] + cost[i-1]),
+                (totalCosts[i-2] + cost[i-2])))
             
-        return min((stairCosts[-1] + cost[-1]),
-                   (stairCosts[-2] + cost[-2]))
+        return min((totalCosts[-1] + cost[-1]),
+                   (totalCosts[-2] + cost[-2]))
